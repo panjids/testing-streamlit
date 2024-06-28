c51340b18fdc8acbd26e3b68c0150ef92289758d
panjids <panjids@gmail.com>
2024-06-28 14:51:25 +0700

    add creadit

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,5 @@ st.altair_chart(alt.Chart(df, height=700, width=700)
         color=alt.Color("idx", legend=None, scale=alt.Scale()),
         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
     ))
+
+st.text('Credit: basic template streamlit')
